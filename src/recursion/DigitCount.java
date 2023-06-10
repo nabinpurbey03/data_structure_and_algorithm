fc206317b578c53fc4054155f4ae1b8b5b4c0065
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-10 17:43:51 +0545

    initial commit

--- src/recursion/DigitCount.java
+++ src/recursion/DigitCount.java
@@ -1,5 +1,6 @@
 package recursion;
 
+//Write a java program to count the digits of a given number using recursion.
 public class DigitCount {
     static int countDigit(int n) {
         return n == 0 ? 0 : 1 + countDigit(n / 10);
