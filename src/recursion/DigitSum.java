ece08f54923cef5f21a3612e8c4e15e41330ef9e
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-10 17:52:24 +0545

    initial commit

---
+++ src/recursion/DigitSum.java
@@ -0,0 +1,11 @@
+package recursion;
+
+public class DigitSum {
+    static int sumDigit(int n) {
+        return n == 0 ? 0 : n % 10 + sumDigit(n / 10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumDigit(9));
+    }
+}
