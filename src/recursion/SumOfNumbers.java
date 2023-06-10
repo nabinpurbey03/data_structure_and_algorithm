f1d2435ca6033e12215503ac0b19dd9abf93595c
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-10 10:00:00 +0545

    initial commit

--- src/recursion/SumOfNumbers.java
+++ src/recursion/SumOfNumbers.java
@@ -1,5 +1,6 @@
 package recursion;
 
+//Write a java program to calculate the sum of numbers from 1 to n using recursion.
 public class SumOfNumbers {
     static int sum(int n) {
         return n == 0 ? 0 : n + sum(n - 1);
