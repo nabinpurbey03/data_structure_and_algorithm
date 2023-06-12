2a2e9becb1effbc0407f88f0ef84f9767876881d
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-12 22:52:08 +0545

    initial commit

--- src/recursion/PrimeNumberCheck.java
+++ src/recursion/PrimeNumberCheck.java
@@ -1,7 +1,25 @@
 package recursion;
 
+//Write a java program to check if a number is a prime number or not using recursion.
 public class PrimeNumberCheck {
-    public static void main(String[] args) {
+    static boolean primeCheck(int num) {
+        return primeCheckRecursion(num, 2);
+    }
 
+    static boolean primeCheckRecursion(int num, int divisor) {
+        if (divisor * divisor > num) {
+            return true;
+        } else {
+            int cur = num % divisor;
+            if (cur == 0) {
+                return false;
+            } else {
+                return primeCheckRecursion(num, divisor + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(primeCheck(29));
     }
 }
