63376af860335666213190c8cabd8d801f32793a
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-14 23:41:40 +0545

    initial commit

--- src/linked_list/CircularlyLinkedLIstMain.java
+++ src/linked_list/CircularlyLinkedLIstMain.java
@@ -1,25 +1,27 @@
 package linked_list;
 
+import linked_list.LinkedListInterface.LinkedList;
+
 /**
- * Singly Linked List node class
+ * Circularly Linked List node class
  */
-class SLLNode<T> {
+class CLLNode<T> {
     T info;
 
-    public SLLNode(T info) {
+    public CLLNode(T info) {
         this.info = info;
     }
 
-    SLLNode<T> next;
+    CLLNode<T> next;
 }
 
-class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
-    private SLLNode<T> first, last;
+class CircularlyLinkedList<T> implements LinkedList<T> {
+    private CLLNode<T> first, last;
     private int size = 0;
 
     @Override
     public void insertFirst(T data) {
-        SLLNode<T> newNode = new SLLNode<>(data);
+        CLLNode<T> newNode = new CLLNode<>(data);
         if (first == null) {
             first = newNode;
             last = newNode;
@@ -27,18 +29,20 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
             newNode.next = first;
             first = newNode;
         }
+        last.next = first;
         size++;
     }
 
     @Override
     public void insertLast(T data) {
-        SLLNode<T> newNode = new SLLNode<>(data);
+        CLLNode<T> newNode = new CLLNode<>(data);
         if (first == null) {
             first = newNode;
         } else {
             last.next = newNode;
         }
         last = newNode;
+        last.next = first;
         size++;
     }
 
@@ -53,8 +57,8 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
         } else if (index == size) {
             insertLast(data);
         } else {
-            SLLNode<T> newNode = new SLLNode<>(data);
-            SLLNode<T> temp = first;
+            CLLNode<T> temp = first;
+            CLLNode<T> newNode = new CLLNode<>(data);
             for (int i = 1; i < index; i++) {
                 temp = temp.next;
             }
@@ -68,7 +72,7 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
     public void search(T data) {
         int position = 0;
         boolean isFound = false;
-        SLLNode<T> temp = first;
+        CLLNode<T> temp = first;
         for (int i = 0; i < size; i++) {
             if (temp.info == data) {
                 System.out.println("Search successful: " + temp.info + " found at index " + position);
@@ -95,7 +99,7 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
             return deletedItem;
         } else {
             T deletedItem = first.info;
-            SLLNode<T> temp = first;
+            CLLNode<T> temp = first;
             first = temp.next;
             temp.next = null;
             size--;
@@ -108,19 +112,20 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
         if (first == null) {
             return null;
         } else if (first == last) {
-            T deletedItem = first.info;
+            T deletedItem = last.info;
             first = null;
             last = null;
             size--;
             return deletedItem;
         } else {
             T deletedItem = last.info;
-            SLLNode<T> temp = first;
+            CLLNode<T> temp = first;
             while (temp.next != last) {
                 temp = temp.next;
             }
-            last = temp;
             last.next = null;
+            temp.next = first;
+            last = temp;
             size--;
             return deletedItem;
         }
@@ -128,7 +133,7 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
 
     @Override
     public T deleteAt(int index) {
-        if (index > size - 1 || index < 0) {
+        if (index > size || index < 0) {
             System.out.println("Cannot delete data. Please input right index [from 0 to " + (size - 1) + "].");
             return null;
         }
@@ -137,14 +142,15 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
         } else if (index == size - 1) {
             return deleteLast();
         } else {
-            SLLNode<T> temp = first;
+            CLLNode<T> temp = first;
             for (int i = 1; i < index; i++) {
                 temp = temp.next;
             }
-            SLLNode<T> delNode = temp.next;
+            CLLNode<T> delNode = temp.next;
             T deletedItem = delNode.info;
             temp.next = temp.next.next;
             delNode.next = null;
+            last.next = first;
             size--;
             return deletedItem;
         }
@@ -160,44 +166,35 @@ class SinglyLinkedList<T> implements LinkedListInterface.LinkedList<T> {
         if (first == null) {
             System.out.println("Empty list");
         } else {
-            SLLNode<T> temp = first;
-            while (temp != null) {
+            CLLNode<T> temp = first;
+            while (temp != last) {
                 System.out.print(temp.info + " ");
                 temp = temp.next;
             }
-            System.out.println();
+            System.out.println(temp.info);
         }
     }
 }
 
-public class SinglyLinkedListMain {
+public class CircularlyLinkedLIstMain {
     public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-        list.insertFirst(8);
-        list.insertLast(10);
-        list.insertLast(20);
-        list.insertLast(30);
+        CircularlyLinkedList<String> list = new CircularlyLinkedList<>();
+        list.insertFirst("Nabin");
+        list.insertLast("Purbey");
         list.display();
-        System.out.println(list.deleteFirst());
+        System.out.println(list.getSize());
+        list.insertFirst("BIM");
         list.display();
         System.out.println(list.deleteLast());
         list.display();
-        list.insertAt(1, 52);
+        System.out.println(list.deleteFirst());
         list.display();
-        list.insertAt(0, 25);
+        list.insertAt(1, "CSIT");
+        list.insertAt(1, "Swing");
         list.display();
-        list.insertAt(3, 25);
-        list.display();
-        list.insertAt(2, 25);
-        list.display();
-        System.out.println(list.deleteAt(0));
-        list.display();
-        System.out.println(list.deleteAt(4));
-        list.display();
+        System.out.println(list.getSize());
         System.out.println(list.deleteAt(1));
         list.display();
-        list.search(8);
-        list.search(25);
-        System.out.println(list.getSize());
+        list.search("CSIT");
     }
 }
