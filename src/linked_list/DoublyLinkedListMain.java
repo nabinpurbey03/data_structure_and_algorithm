a0472edecec3b4313fc0afa1aa879adfd3d92f26
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-15 19:14:32 +0545

    initial commit

---
+++ src/linked_list/DoublyLinkedListMain.java
@@ -0,0 +1,59 @@
+package linked_list;
+import linked_list.LinkedListInterface.LinkedList;
+
+class DLLNode<T>{
+    T info;
+    DLLNode<T> next, prev;
+}
+class DoublyLinkedList<T> implements LinkedList<T>{
+
+    @Override
+    public void insertFirst(T data) {
+
+    }
+
+    @Override
+    public void insertLast(T data) {
+
+    }
+
+    @Override
+    public void insertAt(int index, T data) {
+
+    }
+
+    @Override
+    public void search(T data) {
+
+    }
+
+    @Override
+    public T deleteFirst() {
+        return null;
+    }
+
+    @Override
+    public T deleteLast() {
+        return null;
+    }
+
+    @Override
+    public T deleteAt(int index) {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void display() {
+
+    }
+}
+public class DoublyLinkedListMain {
+    public static void main(String[] args) {
+
+    }
+}
