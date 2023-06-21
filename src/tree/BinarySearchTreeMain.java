b9537a06d40e388e9983f6b57b33f4a8344984e2
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-06-21 19:37:19 +0545

    initial commit

---
+++ src/tree/BinarySearchTreeMain.java
@@ -0,0 +1,54 @@
+package tree;
+
+class BSTNode {
+    int info;
+
+    public BSTNode(int info) {
+        this.info = info;
+    }
+
+    BSTNode left, right;
+}
+
+class BinarySearchTree {
+    private BSTNode root;
+
+    private BSTNode insert(BSTNode node, int data) {
+        if (node == null) {
+            node = new BSTNode(data);
+        } else if (data < node.info) {
+            node.left = insert(node.left, data);
+        } else {
+            node.right = insert(node.right, data);
+        }
+        return node;
+    }
+
+    public void insert(int data) {
+        root = insert(root, data);
+    }
+
+    private void preorderTraverse(BSTNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.info + " ");
+        preorderTraverse(root.left);
+        preorderTraverse(root.right);
+    }
+
+    public void preorderTraverse() {
+        preorderTraverse(root);
+    }
+
+}
+
+public class BinarySearchTreeMain {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(10);
+        tree.insert(12);
+        tree.insert(8);
+        tree.preorderTraverse();
+    }
+}
