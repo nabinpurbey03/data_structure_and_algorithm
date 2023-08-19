7275430fb505e2ad520a676f5cefb8b49034aa94
Nabin Purbey <nabinpurbey03@hotmail.com>
2023-08-19 19:11:45 +0545

    initial commit

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -4,30 +4,25 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSort {
-    static int partition(int[] a, int low, int high){
+    private static int partition(int[] a, int low, int high) {
         int pivot = a[low];
-        int i=low, j=high;
-        while (i<j){
+        int i = low, j = high;
+        while (i < j) {
             do {
                 i++;
-            }while (a[i]<pivot);
+            } while (a[i] < pivot);
             do {
                 j--;
-            }while (a[j]>pivot);
-            if(i<j){
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
+            } while (a[j] > pivot);
+            if (i < j)
+                swap(a, i, j);
         }
-        int temp = a[low];
-        a[low] = a[j];
-        a[j] = temp;
+        swap(a, low, j);
 
         return j;
     }
 
-    static int[] quicksort(int[] a, int low, int high) {
+    private static int[] quicksort(int[] a, int low, int high) {
         if (low < high) {
             int partitionIndex = partition(a, low, high);
             quicksort(a, low, partitionIndex);
@@ -36,7 +31,13 @@ public class QuickSort {
         return a;
     }
 
-    static int[] quicksort(int[] a) {
+    private static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    private static int[] quicksort(int[] a) {
         int[] b = new int[a.length + 1];
         for (int i = 0; i < b.length; i++) {
             if (i == b.length - 1) {
@@ -50,6 +51,7 @@ public class QuickSort {
         System.arraycopy(a, 0, c, 0, a.length - 1);
         return c;
     }
+
     public static void main(String[] args) {
         Random ran = new Random();
         int[] a = new int[100];
